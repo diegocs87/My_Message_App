abcf037c54c4dab13e9e0f6a68aa21fdef5c5a61
diegocs87 <dlcardenas83@gmail.com>
2022-09-04 22:22:43 -0500

    Main pager adapter added on Main Activity

--- app/src/main/java/com/example/mymessageapp/view/MainActivity.kt
+++ app/src/main/java/com/example/mymessageapp/view/MainActivity.kt
@@ -3,14 +3,17 @@ package com.example.mymessageapp.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mymessageapp.databinding.ActivityMainBinding
+import com.example.mymessageapp.view.adapters.MainPagerAdapter
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mainBinding: ActivityMainBinding
+    private val mainPagerAdapter by lazy { MainPagerAdapter(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
+        mainBinding.mainPager.adapter = mainPagerAdapter
     }
 }
