1fec665d3ddf543e972dc9fd0466768355690890
diegocs87 <dlcardenas83@gmail.com>
2022-09-05 11:38:20 -0500

    Tags Detail Acttivity created

--- app/src/main/java/com/example/mymessageapp/view/fragments/AllPostsFragment.kt
+++ app/src/main/java/com/example/mymessageapp/view/fragments/AllPostsFragment.kt
@@ -15,13 +15,7 @@ import com.example.mymessageapp.view.adapters.AllPostsRecyclerAdapter
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AllPostsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AllPostsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -38,7 +32,7 @@ class AllPostsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         PostsFragmentbinding = FragmentAllPostsBinding.inflate(inflater, container, false)
         PostsFragmentbinding!!.allPostsRecyclerView.adapter = AllPostsRecyclerAdapter(fillRecyclerTest())
         { it ->
@@ -59,15 +53,6 @@ class AllPostsFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AllPostsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             AllPostsFragment().apply {
