52c5164a5ac5af8fccf5575ac2c7711f20f24b5d
diegocs87 <dlcardenas83@gmail.com>
2022-09-07 12:26:45 -0500

    Favorite custom delete method added and teted

--- app/src/main/java/com/example/mymessageapp/model/data/database/dao/FavoritesDao.kt
+++ app/src/main/java/com/example/mymessageapp/model/data/database/dao/FavoritesDao.kt
@@ -9,8 +9,14 @@ interface FavoritesDao {
     @Query("SELECT * FROM favorites_table")
     suspend fun getAllFavorites(): List<FavoritesEntity>
 
+    @Query("SELECT * FROM favorites_table WHERE tittle = :tittle")
+    suspend fun getFavoriteStatus(tittle: String): FavoritesEntity
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-     suspend fun insertFavorite(favorites: FavoritesEntity)
+    suspend fun insertFavorite(favorites: FavoritesEntity)
+
+    @Query ("DELETE FROM favorites_table WHERE tittle = :tittle")
+    suspend fun deleteFavoriteTittle(tittle: String)
 
     @Delete
     suspend fun deleteFavorite(posts: FavoritesEntity)
--- app/src/main/java/com/example/mymessageapp/viewmodel/AddFavoriteViewModel.kt
+++ app/src/main/java/com/example/mymessageapp/viewmodel/AddFavoriteViewModel.kt
@@ -2,17 +2,33 @@ package com.example.mymessageapp.viewmodel
 
 import android.app.Application
 import android.content.Context
+import android.content.Entity
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.mymessageapp.model.data.PostsDataItem
 import com.example.mymessageapp.model.data.database.PostsDataBase
 import com.example.mymessageapp.model.data.database.entities.FavoritesEntity
+import com.example.mymessageapp.model.data.database.entities.toPostsData
 import kotlinx.coroutines.launch
 
 class AddFavoriteViewModel(): ViewModel() {
+    lateinit var currentFav: PostsDataItem
+    lateinit var currentFavEntity: FavoritesEntity
     fun getFavorite(favPost: FavoritesEntity, context: Context){
         val favoritesDao = PostsDataBase.buildDatabase(context).favoritesDao()
         viewModelScope.launch {
-            favoritesDao.insertFavorite(favPost)
+            if(favoritesDao.getFavoriteStatus(favPost.tittle) == null){
+                favoritesDao.insertFavorite(favPost)
+                println("adding element")
+            }else{
+                favoritesDao.deleteFavoriteTittle(favPost.tittle)
+                favoritesDao.deleteFavorite(favPost)
+                println("deleting element")
+            }
+
+//            favPost.toPostsData()
+//            currentFavEntity = favoritesDao.getAllFavorites()[0]
+//            currentFav = currentFavEntity.toPostsData()
         }
     }
 }
