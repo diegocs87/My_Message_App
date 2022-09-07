bdbfd17c1cd898499e0457a09e6a38956bf13a9d
diegocs87 <dlcardenas83@gmail.com>
2022-09-06 20:57:18 -0500

    Update all button added

--- app/src/main/java/com/example/mymessageapp/view/fragments/AllPostsFragment.kt
+++ app/src/main/java/com/example/mymessageapp/view/fragments/AllPostsFragment.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
@@ -98,7 +99,7 @@ class AllPostsFragment : Fragment() {
         intent.putExtra(PostsDetailActivity.EXTRA_POST, postData)
         startActivity(intent)
     }
-
+    
     companion object {
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
